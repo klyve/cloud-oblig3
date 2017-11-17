cc3911130343bcfc07f53fc9ed8ff54b00f03f1b
Mortenoj <morteom@stud.ntnu.no>
2017-11-17 01:44:51 +0100

    added structs for data you get from messenger + added handler for storing data in database and returns statuscode 200 OK

--- bot/bot.go
+++ bot/bot.go
@@ -1,6 +1,10 @@
 package bot
 
 import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cloud-oblig3/api"
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2"
 )
@@ -15,4 +19,26 @@ func Init(router *mux.Router, db *mgo.Database) {
 	CreateRoutes()
 	recipe := FindRecipe("hello")
 	Route(recipe)
+	router.HandleFunc("/bot/", FacebookWebHook).Methods("POST")
+
+}
+
+// FacebookWebHook handler for Facebook Webook events
+func FacebookWebHook(w http.ResponseWriter, r *http.Request) {
+	var fbhook FBWebHook
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&fbhook)
+
+	if err != nil {
+		api.ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+		return
+	}
+
+	err2 := database.C("messages").Insert(fbhook)
+	if err2 != nil {
+		api.ErrorWithJSON(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
+
+	api.WriteJSONResponse(w, fbhook)
 }
--- bot/structs.go
+++ bot/structs.go
@@ -19,3 +19,31 @@ type RouterData struct {
 	Message string
 	Count   int
 }
+
+// FBWebHook struct for data from Facebook
+type FBWebHook struct {
+	Object string    `json:"object"`
+	Entry  []Entries `json:"entry"`
+}
+
+// Entries struct for entry data from Facebook
+type Entries struct {
+	ID        string      `json:"id"`
+	Time      int         `json:"time"`
+	Messaging []Messaging `json:"messaging"`
+}
+
+// Messaging struct forn data from facebook
+type Messaging struct {
+	Sender    map[string]string `json:"sender"`
+	Recipient map[string]string `json:"recipient"`
+	Timestamp int64             `json:"timestamp"`
+	Message   Message           `json:"message"`
+}
+
+// Message struct
+type Message struct {
+	Mid  string `json:"mid"`
+	Seq  int64  `json:"seq"`
+	Text string `json:"text"`
+}
